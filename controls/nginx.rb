d3dd41348789b2334506cf67c104674cc9e47a44
Jon Reynolds <jonreynolds@Jons-MacBook-Pro.local>
2020-07-27 22:07:03 -0500

    Couple of checks deleted

--- controls/nginx.rb
+++ controls/nginx.rb
@@ -5,19 +5,13 @@ control 'nginx' do
   describe service('nginx') do
     it { should be_running }
   end
-  describe ssl(port: 80) do
-    it { should be_enabled }
-  end
   describe os.family do
     it { should eq 'debian' }
   end
   describe nginx do
-    its('version') { should eq '1.12.0' }
+    its('version') { should eq '1.14.0' }
   end
   describe nginx do
     its('modules') { should include 'my_module' }
-  end
-  describe nginx do
-    its('openssl_version.date') { should eq '11 Feb 2013' }
   end
 end
