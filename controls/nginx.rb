cb8df4d90911b31bea49cbbd3195c5b0cf45ce1d
Jon Reynolds <jonreynolds@Jons-MacBook-Pro.local>
2020-07-27 22:32:00 -0500

    Deleted invalid tests

--- controls/nginx.rb
+++ controls/nginx.rb
@@ -15,12 +15,6 @@ control 'nginx' do
     its('modules') { should include 'http_ssl' }
   end
   describe nginx do
-    its('sbin_path') { should include 'blah' }
-  end
-  describe nginx do
-    its('openssl_version') { should include 'blah' }
-  end
-  describe nginx do
-    its('compiler_info') { should include 'blah' }
+    its('sbin_path') { should eq 'blah' }
   end
 end
